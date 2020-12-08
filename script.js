abf276f8e785888a38f51aeec0b0641b582eb3d7
Ari Jesus <arimario.jesus@hotmail.com>
2020-12-08 18:59:44 -0300

    update: handle change operator button

--- script.js
+++ script.js
@@ -2,24 +2,27 @@ const result = document.querySelector('#result p');
 const btnAdd = document.querySelector('#btn-add');
 const fieldTimes = document.querySelector('#field-times');
 
-let timeInputIndex = 0;
+let indexField = 1;
 
 const handleCreateTimeInput = () => {
   const trElement = document.createElement('tr');
-  trElement.classList.add('input-wrapper', 'secondary', 'input-' + timeInputIndex++);
+  trElement.classList.add('input-wrapper', 'field-' + indexField);
 
   trElement.innerHTML = `
     <td>
-      <button class="btn operator">-</button>
+      <button class="btn operator" 
+      onclick="changeOperator(this)"
+      data-operator="0"
+      data-index="${indexField}">+</button>
     </td>
     <td>
-      <input type="time" class="input">
+      <input type="time" class="input" step="2" id="index-${indexField}">
     </td>
     <td>
-      <button class="btn btn-delete" onclick="handleRemoveChild()">x</button>
+      <button id="${indexField}" class="btn btn-delete" onclick="handleRemoveChild(this)">x</button>
     </td>
   `;
-
+  indexField++;
   return trElement;
 }
 
@@ -31,14 +34,43 @@ const addField = () => {
 btnAdd.addEventListener('click', addField);
 
 // TODO: Remover Campo
-const handleRemoveChild = () => {
-  const element = document.querySelector('.input-' + (timeInputIndex-1));
-  timeInputIndex--;
+const handleRemoveChild = field => {
+  const indexElement = field.id;
+  const element = document.querySelector('.field-' + (indexElement));
+
   element.remove();
 }
 
 // TODO: Mudar operador
+const nextOperator = (element, index) => {
+  const operators = ['+', '-', 'x', '/'];
+  element.innerHTML = operators[index];
+  element.setAttribute('data-operator', index);
+}
+
+const handleMultiplicationOrDivision = (element, indexOperator) => {
+  const indexInput = element.getAttribute('data-index');
+  const currentInput = document.querySelector('#index-'+indexInput);
+
+  if (indexOperator != 2 && indexOperator != 3){
+    currentInput.setAttribute('type', 'time');
+  }else {
+    currentInput.setAttribute('type', 'number');
+    currentInput.setAttribute('min', '1');
+    currentInput.setAttribute('max', '1000');
+  }
+}
+
+const changeOperator = element => {
+  let indexOperator = element.getAttribute('data-operator');
+  indexOperator = indexOperator < 3 ? ++indexOperator : 0;
+
+  nextOperator(element, indexOperator);
+  handleMultiplicationOrDivision(element, indexOperator);
+}
 
 // TODO: Calcular
 
 // TODO: Adicionar resultado
+
+const elt = document.querySelector('#elt');
